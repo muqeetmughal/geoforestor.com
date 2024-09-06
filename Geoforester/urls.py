c02dc9bac2f65bb539e05693221912a2d319aec8
muqeetmughal <muqeetmughal786@gmail.com>
2024-09-07 03:39:16 +0500

    urls updated

--- Geoforester/urls.py
+++ Geoforester/urls.py
@@ -6,6 +6,6 @@ from django.urls import path,include
 from gisapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('gisapp/',include('gisapp.urls'))
+    path('',include('gisapp.urls'))
 ] 
 # + static(settings.STATIC_URL, document_root=settings.STATIC_URL)
--- gisapp/urls.py
+++ gisapp/urls.py
@@ -3,7 +3,7 @@ from .views import index,login,signup,signout
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path('index',index, name='index'),
+    path('',index, name='index'),
     path("login", login, name="login"),
     path("signup", signup, name="signup"),
     path("signout", signout, name="signout"),
